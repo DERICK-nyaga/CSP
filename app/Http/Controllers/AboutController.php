3137c9d0e162add9635e0c2966d4b41bd7326c8e
DERICK-nyaga <dericknyagam@gmail.com>
2023-06-12 21:42:01 +0300

    fetching data from the about table

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AboutController extends Controller
 {
@@ -11,7 +12,8 @@ class AboutController extends Controller
      */
     public function index()
     {
-        //
+        $about = DB::table('about')->get();
+        return redirect()->route('about');
     }
 
     /**
