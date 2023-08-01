5e57ee1868d3e47e92a3b5ea48ec05d6d934c074
DERICK-nyaga <dericknyagam@gmail.com>
2023-08-01 23:45:58 +0300

    Update allcompanies.blade.php

--- resources/views/users/allcompanies.blade.php
+++ resources/views/users/allcompanies.blade.php
@@ -24,35 +24,6 @@
             <td><a href="{{ route('singlecompany', $company->id) }}">visit me&rarr;</a></td>
         </tr>
             @endforeach
-
-      {{-- <tr>
-        <th scope="row">1</th>
-        <td>Merkel Couriers</td>
-        <td>+358568939</td>
-        <td>mdohdjs@gmail.com</td>
-        <td><a href="{{ route('allbranches') }}">visit me&rarr;</a></td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacabees Fargo</td>
-        <td>+5385909</td>
-        <td>jagabees@gmail.com</td>
-        <td><a href="#">visit me&rarr;</a></td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Markcld Wells Int</td>
-        <td>+643509231809</td>
-        <td>twitter@gmail.com</td>
-        <td><a href="#">visit me&rarr;</a></td>
-      </tr>
-      <tr>
-        <th scope="row">4</th>
-        <td>Mrcloss Courier Int</td>
-        <td>+57387589</td>
-        <td>almdo@gmail.com</td>
-        <td><a href="#">visit me&rarr;</a></td>
-      </tr> --}}
     </tbody>
   </table>
 
