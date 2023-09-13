a7e3cc6d078f8d75540828d1c6fc2d45a117b231
DERICK-nyaga <dericknyagam@gmail.com>
2023-09-13 15:16:31 +0300

    Update ParcelController.php

--- app/Http/Controllers/ParcelController.php
+++ app/Http/Controllers/ParcelController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Session;
 use App\Models\Parcel;
 use Illuminate\Support\Facades\DB;
-// use Illuminate\Support\Facades\Request;
 use Illuminate\Http\Request;
 use App\Models\Branches;
 use App\Models\Company;
